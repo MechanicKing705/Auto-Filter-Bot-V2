c56211a3fdc2e91e674348205569b3e6fabb277e
MechanicKing705 <88239272+MechanicKing705@users.noreply.github.com>
2021-08-06 13:20:33 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,67 +3,27 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
+I Am AutoFilter V4 Bot 
 
-This is Version 2 of Auto Filter Bot
+This Bot Only Work On @Movie_launchers
 
-Bot gives button link to files in connected channels on query !
+No need to add Me In Your Grop . I Dont Work😇
 
-No need to add filters for your files or movies from now on!
-
-For more click <i>help</i></b>"""
+For more click help <i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
 
 <i>
-* Add bot to your group with admin rights.
+* Sorry, This Bot Can Only Be Used In @Movie_launchers..!
 
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
-
-
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+My Owner: @Adolf_hitler_2...</b>"""
 
 
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V4</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @Adolf_hitler_2
 
-⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>Language :</b> <code>Python3</code></a> 
 
 """
